1ce6d39074b05c32030c1b1d3f1ebfd7ef13cc69
Karsten Lehmann <mail@kalehmann.de>
2023-04-29 22:54:42 +0200

    feat: Github link in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -13,22 +13,35 @@ extensions = [
     'sphinxcontrib.jquery',
 ]
 autodoc_mock_imports = [
-    "audiocore",
-    "audiomp3",
-    "audiopwmio",
-    "rotaryio",
-    "usb_hid",
-    "usb_midi",
+    'audiocore',
+    'audiomp3',
+    'audiopwmio',
+    'rotaryio',
+    'usb_hid',
+    'usb_midi',
 ]
 autodoc_typehints = "both"
 
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", None),
-    "CircuitPython": ("https://docs.circuitpython.org/en/latest/", None),
+    'python': (
+        'https://docs.python.org/3',
+        None,
+    ),
+    'CircuitPython': (
+        'https://docs.circuitpython.org/en/latest/',
+        None,
+    ),
 }
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-html_theme = 'sphinx_rtd_theme'
+html_context = {
+    'conf_py_path': '/docs/',
+    'display_github': True,
+    'github_repo': 'onionpad',
+    'github_user': 'kalehmann',
+    'github_version': 'main',
+}
 html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
